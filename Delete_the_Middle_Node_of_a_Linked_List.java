a0bed5812f5614f5b212b0c2abe52842ee2cf369
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-07-27 10:53:31 +0530

    update code edge case

--- Delete_the_Middle_Node_of_a_Linked_List.java
+++ Delete_the_Middle_Node_of_a_Linked_List.java
@@ -22,6 +22,13 @@ public class Delete_the_Middle_Node_of_a_Linked_List {
     class Solution {
         // Delete Middle Node Function
         public ListNode deleteMiddle(ListNode head) {
+            // If, head is null means 0 element in the Linked List
+            // If, head next is null means 1 element in the Linked List
+            // In both case return head
+            if(head == null || head.next == null) {
+                return head;
+            }
+
             // Create 3 variable slow, fast & prev
             // & initialize with head
             ListNode slow = head;
