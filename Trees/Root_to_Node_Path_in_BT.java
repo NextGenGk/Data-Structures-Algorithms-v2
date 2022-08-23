d27474c4d10f265330fd0c1d2a86bfccf36925f2
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-08-23 23:47:16 +0530

    update code

--- Trees/Root_to_Node_Path_in_BT.java
+++ Trees/Root_to_Node_Path_in_BT.java
@@ -75,6 +75,25 @@ Input : 1,2,4,5,6,7,3
 Output : 1,2,5,7
  */
 
+// Approach -
+/*
+We will use an external list to store our path. This list will be passed by reference to our recursive function.
+Moreover, we can set the return value of our function as boolean, this will help us to know whether node V was
+found in a subtree or not.
+
+The algorithm steps can be stated as follows:
+
+1. We pass the function with our root node, the path list and node V.
+2. For the base case, if root is pointing to NULL, we return false as clearly node V can’t be found.
+3. Now we first push the node to our path list.
+4. Then we check whether the current node is the target node or not, if it is then no further execution is needed
+   and we return to the parent function.
+5. If not, then we recursively call its left and right child to find the target node V. If any one of them returns true,
+   it means we have found node V at lower levels and return true from the current function.
+6. If the value is not found at the current node and neither in any of the recursive calls, it means that the value
+   is not present in the current sub-tree, therefore we pop out the current node from the path list and return false.
+ */
+
 // Time & Space Complexity -
 /*
 Time - O(n), We are doing a simple inorder tree traversal.
