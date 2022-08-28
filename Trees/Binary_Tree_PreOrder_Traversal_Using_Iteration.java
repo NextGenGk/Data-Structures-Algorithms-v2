c7e521150cfa6b0b0fdf175c69724c08fa7fb000
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-08-28 11:41:38 +0530

    Add Binary Tree Preorder Traversal Using Iteration

---
+++ Trees/Binary_Tree_PreOrder_Traversal_Using_Iteration.java
@@ -0,0 +1,71 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Binary_Tree_PreOrder_Traversal_Using_Iteration {
+
+
+   // Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+          // Preorder Traversal Function
+        public static List<Integer> preorderTraversal(TreeNode root) {
+            // create a list which stores ans
+            List<Integer> res = new ArrayList<>();
+            // base case
+            if(root == null) return res;
+            // craete a stack
+            Stack<TreeNode> stack = new Stack<>();
+            // added root to the stack
+            stack.push(root);
+
+            // while, stack is not empty
+            while(!stack.isEmpty()) {
+                // remove curr element to the stack
+                // and added to the result list
+                TreeNode curr = stack.pop();
+                res.add(curr.val);
+
+                // if, right & left is not null than
+                // right & left is push on the stack
+                if(curr.right != null) stack.push(curr.right);
+                if(curr.left != null) stack.push(curr.left);
+            }
+            // finally, return list of result
+            return res;
+        }
+    }
+}
+// Output -
+/*
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+*/
+
+// Algorithm -
+/*
+PreOrder - Root -> Left SubTree -> Right SubTree (Recursion)
+PreOrder - root -> right -> left (Iteration)
+*/
+
+// Time & Space Complexity -
+/*
+Time - O(n)
+Space - O(n)
+*/
+
+
