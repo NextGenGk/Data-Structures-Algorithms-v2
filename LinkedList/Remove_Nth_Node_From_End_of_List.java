899ab60d5a87a701d6d37d5e62be52e4f82c9848
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-09-08 23:25:23 +0530

    update code

--- LinkedList/Remove_Nth_Node_From_End_of_List.java
+++ LinkedList/Remove_Nth_Node_From_End_of_List.java
@@ -2,14 +2,14 @@ package LinkedList;
 
 public class Remove_Nth_Node_From_End_of_List {
 
-      // Definition for singly-linked list.
-      public class ListNode {
-          int val;
-          ListNode next;
-          ListNode() {}
-          ListNode(int val) { this.val = val; }
-         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-      }
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
     // Remove Nth Node From End of List Function
     public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -39,7 +39,7 @@ public class Remove_Nth_Node_From_End_of_List {
         // Find Nth Node previous element
         int i=1;
         // looping through, while i is less than delete Nth Node
-        // untill then store the prev.next element on prev variable
+        // until then store the prev.next element on prev variable
         // & increment i by 1
         while(i < deleteNthNode) {
             prev = prev.next;
