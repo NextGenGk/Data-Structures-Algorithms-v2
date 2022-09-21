ba457b0d4d6e0abb1a01a01e1d240e82bb4490a7
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-09-21 20:55:51 +0530

    update code

--- Arrays/Plus_One.java
+++ Arrays/Plus_One.java
@@ -34,12 +34,14 @@ public class Plus_One {
 
 // Output -
 /*
+Example 1 :
 Input: digits = [1,2,3]
 Output: [1,2,4]
 Explanation: The array represents the integer 123.
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 
+Example 2 :
 Input: digits = [9]
 Output: [1,0]
 Explanation: The array represents the integer 9.
