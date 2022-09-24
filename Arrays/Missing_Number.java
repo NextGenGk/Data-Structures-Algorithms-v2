42063bb44c9728a9215fa5c7f4c93b5f5965fedf
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-09-24 23:40:11 +0530

    Add Missing Number

---
+++ Arrays/Missing_Number.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class Missing_Number {
+
+    class Solution {
+        public int missingNumber(int[] nums) {
+            int sum = 0;
+            int n = nums.length;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            return n * (n + 1) / 2 - sum;
+        }
+    }
+}
+
+// Logic sum of N natural numbers n*(n-1)/2
+
+// Output -
+/*
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+*/
+
+// Algorithm -
+/*
+1. First, we initialize a variable sum to 0.
+2. Next, we loop through the array nums and add each element to sum.
+3. Finally, we return n*(n+1)/2 - sum.
+*/
+
+// Time & Space Complexity -
+/*
+Time - O(N)
+Space - O(1)
+*/
