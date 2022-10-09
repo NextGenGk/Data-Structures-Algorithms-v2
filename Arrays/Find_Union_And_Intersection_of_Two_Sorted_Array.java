11c09d047f84a670272b5969aaab1e1ddf275ba4
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-10-09 23:35:58 +0530

    update Find Union & Intersection of Sorted Array

--- Arrays/Find_Union_And_Intersection_of_Two_Sorted_Array.java
+++ Arrays/Find_Union_And_Intersection_of_Two_Sorted_Array.java
@@ -49,6 +49,7 @@ public class Find_Union_And_Intersection_of_Two_Sorted_Array {
     /* Function prints Intersection of arr1[] and arr2[]
        m is the number of elements in arr1[]
        n is the number of elements in arr2[] */
+
     // Find Intersection Function
     static void findInterSection(int[] arr1, int[] arr2, int m, int n) {
         // Initial values i = 0, j = 0
