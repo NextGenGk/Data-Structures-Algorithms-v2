0469b772b4cfb9b898a8093d47af55197c1329c1
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-10-13 17:11:05 +0530

    update Next Permutation

--- Arrays/Next_Permutation.java
+++ Arrays/Next_Permutation.java
@@ -62,7 +62,7 @@ public class Next_Permutation {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-        }
+    }
     }
 }
 
@@ -70,6 +70,21 @@ public class Next_Permutation {
 /*
 Input: nums = [1,2,3]
 Output: [1,3,2]
+*/
+
+// Approach -
+/*
+Step 1: Linearly traverse array from backward such that ith index value of the array is less than (i+1)th index value.
+ Store that index in a variable.
+
+Step 2: If the index value received from step 1 is less than 0. This means the given input array is the largest
+lexicographical permutation. Hence, we will reverse the input array to get the minimum or starting permutation.
+Linearly traverse array from backward. Find an index that has a value greater than the previously found index.
+Store index is another variable.
+
+Step 3: Swap values present in indices found in the above two steps.
+
+Step 4: Reverse array from index+1 where the index is found at step 1 till the end of the array.
 */
 
 // Time & Space Complexity -
