f1e746c1df9b02e89e37fc928d9cacd4aa25466e
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-10-21 23:36:31 +0530

    update Search in a 2D Matrix

--- Arrays/Search_in_a_2D_Matrix.java
+++ Arrays/Search_in_a_2D_Matrix.java
@@ -4,6 +4,8 @@ public class Search_in_a_2D_Matrix {
 
     class Solution {
         public boolean searchMatrix(int[][] matrix, int target) {
+            if (matrix.length == 0) return false;
+
             int n = matrix.length;
             int m = matrix[0].length;
 
