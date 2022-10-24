13d0066e6ceacbd44103f4fe86fb51aadd2cf4fc
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-10-24 21:09:49 +0530

    remove unnecessary code

--- Stack_and_Queues/LRU_Cache.java
+++ Stack_and_Queues/LRU_Cache.java
@@ -73,13 +73,6 @@ public class LRU_Cache {
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- **/
-
 // Time & Space Complexity -
 /*
 Time -
