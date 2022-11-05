9de38804a2f003c78c9216d299e3fe547b547497
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-05 23:25:10 +0530

    Add Single Element in Sorted Array

---
+++ BinarySearch/Single_Element_in_Sorted_Array.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class Single_Element_in_Sorted_Array {
+
+    class Solution {
+        public int singleNonDuplicate(int[] nums) {
+            int low = 0;
+            int high = nums.length - 2;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (mid % 2 == 0) {
+                    if (nums[mid] != nums[mid + 1]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] == nums[mid + 1]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            }
+            return nums[low];
+        }
+    }
+}
