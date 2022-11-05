cee3f6a68909677d9369a37ed725c9ae1f120b98
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-06 00:45:08 +0530

    update Single Element in Sorted Array

--- BinarySearch/Single_Element_in_Sorted_Array.java
+++ BinarySearch/Single_Element_in_Sorted_Array.java
@@ -40,10 +40,14 @@ Output: 2
 1. We first initialize low and high to 0 and nums.length-2 respectively.
 2. We then enter a while loop with the condition low <= high.
 3. We initialize mid to low + (high - low) / 2.
-4. If mid is even, we check if nums[mid] != nums[mid+1]. If this is true, it means that the element at index mid is the single element that occurs once. So we update high to mid-1.
-5. If this is not true, it means that the elements between nums[mid] and nums[mid+1] occur twice. So we update low to mid+1.
-6. If mid is odd, we check if nums[mid] == nums[mid+1]. If this is true, it means that the elements between nums[mid] and nums[mid+1] occur twice. So we update high to mid-1.
-7. If this is not true, it means that the element at index mid is the single element that occurs once. So we update low to mid+1.
+4. If mid is even, we check if nums[mid] != nums[mid+1]. If this is true, it means that the element at index mid is
+the single element that occurs once. So we update high to mid-1.
+5. If this is not true, it means that the elements between nums[mid] and nums[mid+1] occur twice.
+ So we update low to mid+1.
+6. If mid is odd, we check if nums[mid] == nums[mid+1]. If this is true, it means that the elements between
+ nums[mid] and nums[mid+1] occur twice. So we update high to mid-1.
+7. If this is not true, it means that the element at index mid is the single element that occurs once.
+ So we update low to mid+1.
 8. After the while loop terminates, we return nums[low].
 */
 
