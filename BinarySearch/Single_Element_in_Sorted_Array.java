e4efd134a7214eaa6d62d2dc00ce858ecf5c578f
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-07 23:47:40 +0530

    update Single Element in Sorted Array

--- BinarySearch/Single_Element_in_Sorted_Array.java
+++ BinarySearch/Single_Element_in_Sorted_Array.java
@@ -95,10 +95,10 @@ Output: 2
 
 // Idea -
 /*
-* In order to check for the left half
-// 1st Instance - Even Index
-// 2nd Instance - Odd Index, i need to check on the left, if the 1st instance of that number is there o not
-*/
+ * In order to check for the left half
+ * 1st Instance - Even Index
+ * 2nd Instance - Odd Index, I need to check on the left, if the 1st instance of that number is there o not
+ */
 
 // Time & Space Complexity -
 /*
