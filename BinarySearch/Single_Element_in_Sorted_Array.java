f0073373a3bcea59d108a8ede86d4c00b4473c71
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-07 23:47:02 +0530

    Add New Approach on Single Element in Sorted Array

--- BinarySearch/Single_Element_in_Sorted_Array.java
+++ BinarySearch/Single_Element_in_Sorted_Array.java
@@ -56,3 +56,52 @@ the single element that occurs once. So we update high to mid-1.
 Time - O(log*N)
 Space - O(1)
 */
+
+// New Approach
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 2;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return nums[low];
+    }
+}
+
+// Output -
+/*
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+*/
+
+// Algorithm -
+/*
+1. We initialize low and high to the first and last index of the array.
+2. We initialize mid to the middle index of the array.
+3. If nums[mid] == nums[mid ^ 1], then we know that the single element is on the left side of the middle element.
+4. If nums[mid] != nums[mid ^ 1], then we know that the single element is on the right side of the middle element.
+5. We then update low and high accordingly.
+6. We then return the value of low.
+*/
+
+// Idea -
+/*
+* In order to check for the left half
+// 1st Instance - Even Index
+// 2nd Instance - Odd Index, i need to check on the left, if the 1st instance of that number is there o not
+*/
+
+// Time & Space Complexity -
+/*
+Time - O(log*N)
+Space - O(1)
+*/
