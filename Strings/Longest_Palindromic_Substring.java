a7fbae7d47034a8236eea22d1af3aa1b69092715
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-23 23:14:41 +0530

    update Longest Palindromic Substring

--- Strings/Longest_Palindromic_Substring.java
+++ Strings/Longest_Palindromic_Substring.java
@@ -4,7 +4,7 @@ public class Longest_Palindromic_Substring {
 
     class Solution {
         public String longestPalindrome(String s) {
-            // calculate/mark all palindromic substring
+            // calculate / mark all palindromic substring
             int start = 0;
             int end = 0;
             for (int i = 0; i < s.length(); i++) {
