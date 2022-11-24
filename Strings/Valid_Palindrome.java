8176269f12770caced88505b2c1e8ca355e697ed
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-24 23:37:43 +0530

    update Valid Palindrome

--- Strings/Valid_Palindrome.java
+++ Strings/Valid_Palindrome.java
@@ -16,7 +16,6 @@ class Solution {
                 b.append(s.charAt(i));
             }
         }
-
         // Store last value in n variable
         int n = b.length() - 1;
         // Looping through half of the string element
