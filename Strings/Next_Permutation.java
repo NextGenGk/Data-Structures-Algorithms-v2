179069f92cbd6c3c54c56fafc1d14275b87e0063
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-11-29 19:57:13 +0530

    update Next Permutation

--- Strings/Next_Permutation.java
+++ Strings/Next_Permutation.java
@@ -66,6 +66,52 @@ public class Next_Permutation {
             nums[j] = temp;
         }
     }
+
+// New Method (C++) -
+//    public void nextPermutation(int[] nums) {
+//         if(nums.length==1){
+//             return;
+//         }
+
+//         int idx1 = 0;
+//         for(int i=nums.length-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 idx1=i;
+//                 break;
+//             }
+//         }
+
+//         if(idx1 < 0){
+//             reverse(nums,0,nums.length-1);
+//         }
+//         else {
+//             int idx2=0;
+//             for(int i=nums.length-1;i>=0;i--){
+//                 if(nums[i]>nums[idx1]){
+//                     idx2=i;
+//                     break;
+//                 }
+//             }
+
+//         swap(nums,idx1,idx2);
+
+//         reverse(nums,idx1+1,nums.length-1);
+//         }
+//     }
+
+//     public void reverse(int[] nums,int start,int end){
+//         while(start<end){
+//             swap(nums,start,end);
+//             start++;
+//             end--;
+//         }
+//     }
+
+//     public void swap(int[] nums,int i,int j){
+//         int temp=nums[i];
+//         nums[i]=nums[j];
+//         nums[j]=temp;
+//     }
 }
 
 // Output -
