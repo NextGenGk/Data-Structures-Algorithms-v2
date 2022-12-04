1553445d6b4498f54ee12f99849f46bb2658dbb8
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-04 23:56:45 +0530

    update Count Reversals

--- Strings/Count_Reversals.java
+++ Strings/Count_Reversals.java
@@ -25,3 +25,28 @@ public class Count_Reversals {
         }
     }
 }
+
+// Output -
+/*
+Input:
+S = "}{{}}{{{"
+Output: 3
+Explanation: One way to balance is:
+"{{{}}{}}". There is no balanced sequence
+that can be formed in lesser reversals.
+ */
+
+// Algorithm -
+/*
+1. We first check if the string has an odd number of characters. If it does, we return -1.
+2. We initialize two variables, open and close, to keep track of the number of opening and closing braces.
+3. We loop through the string and increment or decrement the open and close variables accordingly.
+4. We calculate the number of reverse braces by taking the ceiling of the number of open braces divided by 2 and adding it to the ceiling of the number of close braces divided by 2.
+5. We return the number of reverse braces.
+ */
+
+// Time & Space Complexity -
+/*
+Time - O(N)
+Space - O(1)
+ */
