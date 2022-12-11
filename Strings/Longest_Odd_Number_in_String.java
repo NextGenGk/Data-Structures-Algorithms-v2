e8c071f0a9f85c8de445022785d0beaa6aee6b18
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-12 01:02:28 +0530

    update Longest Odd Number in String

--- Strings/Longest_Odd_Number_in_String.java
+++ Strings/Longest_Odd_Number_in_String.java
@@ -17,9 +17,15 @@ public class Longest_Odd_Number_in_String {
 
 // Output -
 /*
+Ex 1 :
 Input: num = "35427"
 Output: "35427"
 Explanation: "35427" is already an odd number.
+
+Ex 2 :
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
 */
 
 // Algorithm -
