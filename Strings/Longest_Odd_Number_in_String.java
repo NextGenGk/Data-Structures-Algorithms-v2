6359529cb05b18697a3afdede267a57662eb6527
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-12 00:58:54 +0530

    Add Longest Odd Number in String

---
+++ Strings/Longest_Odd_Number_in_String.java
@@ -0,0 +1,38 @@
+package Strings;
+
+public class Longest_Odd_Number_in_String {
+
+    class Solution {
+        public String largestOddNumber(String num) {
+            for (int i = num.length() - 1; i >= 0; i--) {
+                char c = num.charAt(i);
+                if (c % 2 == 1) {
+                    return num.substring(0, i + 1);
+                }
+            }
+            return "";
+        }
+    }
+}
+
+// Output -
+/*
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+*/
+
+// Algorithm -
+/*
+1. We iterate through the string from the end to the beginning.
+2. We check if the character at the current index is odd.
+3. If it is odd, we return the substring from the beginning to the current index.
+4. If it is not odd, we continue to the next index.
+5. If we reach the end of the string and haven’t found an odd character, we return an empty string.
+*/
+
+// Time & Space Complexity -
+/*
+Time - O(N), For linearly traversing the string
+Space - O(1), no extra space is needed
+*/
