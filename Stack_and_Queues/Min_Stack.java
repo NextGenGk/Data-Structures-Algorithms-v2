d8d4da019949d7ea35d5593dfbc2537aec0fc4e4
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-24 12:23:00 +0530

    update Min Stack

--- Stack_and_Queues/Min_Stack.java
+++ Stack_and_Queues/Min_Stack.java
@@ -87,18 +87,19 @@ put it in the stack and update the variable to the number.
 
 Push operation:
 Now if there is a push operation just check whether that number is less than the min number.
-If it is smaller than min we will push a modified value which is a push(2 * Val – min) into the stack and will update min to the value of the original number. If it’s not then we will just push it as it is.
+If it is smaller than min we will push a modified value which is a push(2 * Val – min) into the stack and will
+update min to the value of the original number. If it’s not then we will just push it as it is.
 
-getMin() operation:
-We will just return the value of min.
+Pop operation:
+While making pop we will check if the top value is lesser than min, If it is then we must update our min to its
+previous value. In order to do that min = (2 * min) – (modified value) and we will pop the element.
 
 Top operation:
 While returning the top value we know that it is a modified value. We will check if the top value is lesser than min,
 If it is then we will return the min as the top value.
 
-Pop operation:
-While making pop we will check if the top value is lesser than min, If it is then we must update our min to its
-previous value. In order to do that min = (2 * min) – (modified value) and we will pop the element.
+getMin() operation:
+We will just return the value of min.
 */
 
 // Time & Space Complexity -
@@ -107,7 +108,7 @@ Time - O(1)
 Space - O(N)
 */
 
-// Brute Force
+// Brute Force Approach
 
     class MinStack1 {
         Stack<Pair> stk;
