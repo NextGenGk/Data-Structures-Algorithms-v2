215b97b6879128cb6836cd1f13e983a63448cd10
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-31 21:28:37 +0530

    update Next Smaller Element

--- Stack_and_Queues/Next_Smaller_Element.java
+++ Stack_and_Queues/Next_Smaller_Element.java
@@ -69,15 +69,12 @@ Space - O(1)
 Input: N = 5, arr[] = {3, 8, 5, 2, 25}
 Output: 2 5 2 -1 -1
 Explanation:
-1. Roll number 1 has 3 marks. The first person
-who has less marks than him is roll number 4,
+1. Roll number 1 has 3 marks. The first person who has less marks than him is roll number 4,
 who has 2 marks.
-2. Roll number 2 has 8 marks, he helps student
-with 5 marks.
-3. Roll number 3 has 5 marks, he helps student
-with 2 marks.
-4. Roll number 4 and 5 can not pick anyone as
-no student with higher roll number has lesser marks than them. This is denoted by -1.
+2. Roll number 2 has 8 marks, he helps student with 5 marks.
+3. Roll number 3 has 5 marks, he helps student with 2 marks.
+4. Roll number 4 and 5 can not pick anyone as no student with higher roll number has lesser marks than them.
+This is denoted by -1.
 Output shows the marks of the weaker student that each roll number helps in order. ie- 2,5,2,-1,-1
  */
 
