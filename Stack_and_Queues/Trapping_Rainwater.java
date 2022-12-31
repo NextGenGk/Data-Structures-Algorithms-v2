8c8808d1fd97922b6cecf35f88a8f529bb63e35a
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-12-31 22:17:15 +0530

    update Trapping Rainwater

--- Stack_and_Queues/Trapping_Rainwater.java
+++ Stack_and_Queues/Trapping_Rainwater.java
@@ -11,14 +11,14 @@ public class Trapping_Rainwater {
         // Right aux array (& initialize with length of the array)
         int[] right = new int[n];
 
-        // left[i] contains height of tallest bar to the
+        // left[i] contains height of the tallest bar to the
         // left of i'th bar including itself
         left[0] = height[0];
         for (int i = 1; i < n; i++) {
             left[i] = Math.max(left[i - 1], height[i]);
         }
 
-        // Right [i] contains height of tallest bar to
+        // Right [i] contains height of the tallest bar to
         // the right of ith bar including itself
         right[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
@@ -35,7 +35,7 @@ public class Trapping_Rainwater {
         for (int i = 0; i < n; i++) {
             water = water + (Math.min(left[i], right[i]) - height[i]);
         }
-        // finally return max trap water
+        // finally, return max trap water
         return water;
     }
 }
@@ -51,8 +51,8 @@ Explanation: The above elevation map (black section) is represented by array [0,
 // Algorithm -
 /*
 1. Create two arrays left and right of size n.
-2. left[i] contains height of tallest bar to the left of i'th bar including itself
-3. Right [i] contains height of tallest bar to the right of ith bar including itself
+2. left[i] contains height of the tallest bar to the left of i'th bar including itself
+3. Right [i] contains height of the tallest bar to the right of ith bar including itself
 4. Initialize result
 5. Calculate the accumulated water element by element consider the amount of water on i'th bar,
  the amount of water accumulated on this particular bar will be equal to min(left[i], right[i]) - arr[i] .
