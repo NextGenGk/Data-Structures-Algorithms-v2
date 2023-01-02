afcb7485fc53a24e21ee7bfdf56a5a1bcf806c8c
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-01-02 21:41:57 +0530

    update Largest Rectangle in Histogram

--- Stack_and_Queues/Largest_Rectangle_in_Histogram.java
+++ Stack_and_Queues/Largest_Rectangle_in_Histogram.java
@@ -39,7 +39,20 @@ public class Largest_Rectangle_in_Histogram {
                 maxA = Math.max(maxA, heights[i] * (rightSmall[i] - leftSmall[i] + 1));
             }
             return maxA;
-
         }
     }
 }
+
+// Output -
+/*
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+ */
+
+// Time & Space Complexity -
+/*
+Time - O( N )
+Space - O(3N) where 3 is for the stack, left small array and a right small array
+ */
