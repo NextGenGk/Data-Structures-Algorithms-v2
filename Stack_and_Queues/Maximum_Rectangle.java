41a47cfdc1a773598dd9ae49a180940b207abfb3
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-01-04 23:55:01 +0530

    Add Maximum Rectangle

--- Stack_and_Queues/Maximum_Rectangle.java
+++ Stack_and_Queues/Maximum_Rectangle.java
@@ -3,6 +3,26 @@ package Stack_and_Queues;
 public class Maximum_Rectangle {
 
     class Solution {
+        public int maximalRectangle(char[][] matrix) {
+            if (matrix.length == 0) return 0;
+            // for each cell with value=1,
+            // we look upward (north), the number of continuous '1' is the height of cell
+            int[] heights = new int[matrix[0].length];
+            int maxArea = -1;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (matrix[i][j] == '0') {
+                        heights[j] = 0;
+                    } else {
+                        heights[j]++;
+                    }
+                }
+                int area = largestRectangleArea(heights);
+                maxArea = Math.max(maxArea, area);
+            }
+            return maxArea;
+        }
+
         // Largest Rectangle in Histogram
         public static int largestRectangleArea(int[] heights) {
             int n = heights.length;
@@ -44,26 +64,6 @@ public class Maximum_Rectangle {
                 totalUnits = Math.max(totalUnits, (rightSmall[i] - leftSmall[i] + 1) * heights[i]);
             }
             return totalUnits;
-        }
-
-        public int maximalRectangle(char[][] matrix) {
-            if (matrix.length == 0) return 0;
-            // for each cell with value=1,
-            // we look upward (north), the number of continuous '1' is the height of cell
-            int[] heights = new int[matrix[0].length];
-            int maxArea = -1;
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[0].length; j++) {
-                    if (matrix[i][j] == '0') {
-                        heights[j] = 0;
-                    } else {
-                        heights[j]++;
-                    }
-                }
-                int area = largestRectangleArea(heights);
-                maxArea = Math.max(maxArea, area);
-            }
-            return maxArea;
         }
     }
 
