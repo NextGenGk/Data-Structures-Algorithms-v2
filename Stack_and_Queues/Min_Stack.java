3721166950a4e016485f75f4993e8d923615e8b3
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-01-13 16:37:24 +0530

    update Min Stack

--- Stack_and_Queues/Min_Stack.java
+++ Stack_and_Queues/Min_Stack.java
@@ -115,7 +115,6 @@ Space - O(N), because of using stack data structure
         public MinStack1() {
             stk = new Stack<Pair>();
         }
-
         public void push(int val) {
             int min;
             if (stk.isEmpty()) {
