1ba923ee1883d694adbacef27aab6dec1e8d6f73
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-01-22 23:06:51 +0530

    Add Stack in Stack & Queues

---
+++ Stack_and_Queues/Stacks/Stack.java
@@ -0,0 +1,24 @@
+package Stack_and_Queues.Stacks;
+
+public class Stack {
+    LinkedList.SinglyLinkedList linkedList;
+
+    // Constructor
+    public Stack() {
+        linkedList = new LinkedList.SinglyLinkedList();
+    }
+
+    // Main Function
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+    }
+
+    // Push Method - O(1)
+    public void push(int value) {
+        linkedList.insert(value, 0);
+        System.out.println("Successfully Inserted " + value);
+    }
+}
