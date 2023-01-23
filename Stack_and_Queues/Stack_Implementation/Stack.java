ecb81c678ca5f622de8f8df6bb83f8b55ec1ebcb
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-01-23 22:20:56 +0530

    Add Stack Methods using Linked List

--- Stack_and_Queues/Stack_Implementation/Stack.java
+++ Stack_and_Queues/Stack_Implementation/Stack.java
@@ -11,14 +11,57 @@ public class Stack {
     // Main Function
     public static void main(String[] args) {
         Stack stack = new Stack();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+//        stack.push(10);
+//        stack.push(20);
+
+//        int result = stack.pop();
+//        System.out.println(result);
+
+//        boolean result = stack.isEmpty();
+//        System.out.println(result);
+
+//        int result = stack.peek();
+//        System.out.println(result);
+
+//        stack.deleteStack();
     }
 
     // Push Method - O(1)
     public void push(int value) {
         linkedList.insert(value, 0);
         System.out.println("Successfully Inserted " + value);
+    }
+
+    // IsEmpty Method - O(1)
+    public boolean isEmpty() {
+        return linkedList.head == null;
+    }
+
+    // Pop Method - O(1)
+    public int pop() {
+        int result = -1;
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        } else {
+            result = linkedList.head.value;
+            linkedList.delete(0);
+        }
+        return result;
+    }
+
+    // Peek Method - O(1)
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            return linkedList.head.value;
+        }
+    }
+
+    // Delete Method - O(1)
+    public void deleteStack() {
+        linkedList.head = null;
+        System.out.println("Stack is Deleted Successfully");
     }
 }
