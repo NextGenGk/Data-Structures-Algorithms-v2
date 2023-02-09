d05f8ecde9a893da2fbb8ab734395443d8a05f23
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-02-09 20:17:37 +0530

    Add New method on Rotten Oranges

--- Stack_and_Queues/Rotten_Oranges.java
+++ Stack_and_Queues/Rotten_Oranges.java
@@ -81,3 +81,71 @@ Space Complexity: O ( n x n )
 
 Reason: worst-case –  If all oranges are Rotten, we will end up pushing all rotten oranges into the Queue data structure
 */
+
+// New Method -
+
+class Solution {
+    public static int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+        //Put the position of all rotten oranges in queue
+        //count the number of fresh oranges
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+                if (grid[i][j] != 0) {
+                    count_fresh++;
+                }
+            }
+        }
+
+        if (count_fresh == 0) return 0;
+        int countMin = 0, cnt = 0;
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        // bfs starting from initially rotten oranges
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            cnt += size;
+            for (int i = 0; i < size; i++) {
+                int[] point = queue.poll();
+                for (int j = 0; j < 4; j++) {
+                    int x = point[0] + dx[j];
+                    int y = point[1] + dy[j];
+
+                    if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 ||
+                            grid[x][y] == 2) continue;
+
+                    grid[x][y] = 2;
+                    queue.offer(new int[]{x, y});
+                }
+            }
+            if (queue.size() != 0) {
+                countMin++;
+            }
+        }
+        return count_fresh == cnt ? countMin : -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+        int rotting = orangesRotting(arr);
+        System.out.println("Minimum Number of Minutes Required " + rotting);
+    }
+}
+
+// Algorithm -
+/*
+1. We start with a queue of all the rotten oranges.
+2. We keep a count of fresh oranges.
+3. We bfs through the rotten oranges and mark the adjacent oranges as rotten.
+4. We increment the count of minutes by 1.
+5. We keep doing this until there are no rotten oranges left in the queue.
+6. If count of fresh oranges is zero, we return the minutes. Else, we return -1.
+ */
