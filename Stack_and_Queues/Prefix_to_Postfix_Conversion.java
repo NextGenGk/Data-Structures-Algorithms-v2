2376f2f948d0511bfc7a8c196e081d58f0f96ca4
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-02-11 11:37:02 +0530

    update Prefix to Postfix Conversion

--- Stack_and_Queues/Prefix_to_Postfix_Conversion.java
+++ Stack_and_Queues/Prefix_to_Postfix_Conversion.java
@@ -4,20 +4,27 @@ import java.util.Stack;
 
 public class Prefix_to_Postfix_Conversion {
 
+    // Function to check if character
+    // is operator or not
     static boolean isOperator(char ch) {
         switch (ch) {
             case '+':
             case '-':
             case '*':
             case '/':
+            case '^':
+            case '%':
                 return true;
         }
         return false;
     }
 
+    // Convert prefix to Infix expression
     public static String convert(String str) {
-        Stack<String> stack = new Stack<>();
+        // Create a stack
+        Stack<String> stack = new Stack<String>();
 
+        // Reading from right to left
         for (int i = str.length() - 1; i >= 0; i--) {
             char c = str.charAt(i);
 
@@ -25,15 +32,18 @@ public class Prefix_to_Postfix_Conversion {
                 String op1 = stack.pop();
                 String op2 = stack.pop();
 
-                String temp = (op1 + op2 + c);
+                // Concat the operands and operator
+                String temp = (op1 + op2 + c);  // "(" + op1 + c + op2 + ")"; to (op1 + op2 + c)
                 stack.push(temp);
             } else {
+                // To make character to string
                 stack.push(c + "");
             }
         }
-        return stack.peek();
+        return stack.peek(); // change pop() to peek()
     }
 
+    // Main Function
     public static void main(String[] args) {
         String str = "*+AB-CD";
         System.out.println("Postfix : " + convert(str));
