871d56b163baeda431ecbd7b37497b78be54424e
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2023-04-15 22:02:13 +0530

    update Count_Digits

--- Practice/Maths/Count_Digits.java
+++ Practice/Maths/Count_Digits.java
@@ -18,3 +18,14 @@ public class Count_Digits {
         System.out.println(result);
     }
 }
+
+// Algorithm -
+/*
+1. It initializes a variable called count to 0. This variable will keep track of the number of digits in the input integer n.
+2. The code enters a while loop that continues as long as n is greater than 0.
+3. Inside the loop, it finds the last digit of the number n by calculating the remainder of n divided by 10. This is done using the modulus operator %. For example, if n is 123, then lastDigits will be 3 on the first iteration of the loop.
+4. It increments the count variable by 1, indicating that it has found one more digit in the input number.
+5. It updates the value of n by dividing it by 10, discarding the last digit that was just found. For example, if n was 123 and the last digit was 3, then n is updated to 12 on the first iteration of the loop.
+6. The loop repeats until n is less than or equal to 0, at which point it exits the loop.
+7. The method returns the final value of count, which represents the number of digits in the original input integer n.
+ */
